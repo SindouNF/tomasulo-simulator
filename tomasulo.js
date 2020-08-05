54495f65a3743b763fbf6c787de158312a4254db
noirecat <16693325+noirecat@users.noreply.github.com>
2020-08-05 16:13:31 -0300

    novos métodos nas classes exeunit e tomasulo, novos comentários

--- tomasulo.js
+++ tomasulo.js
@@ -12,7 +12,6 @@ let config = {
     "DADDUIcycles": 0,
     "BEQcycles": 0,
     "BNEZcycles": 0,
-    "qtyInstr": 0,
     "exec": "fast" // fast or slow
 };
 
@@ -23,6 +22,8 @@ class Instruction {
         this.opcode = inst.split(" ")[0];
         this.resto = inst.split(" ").split(","); // guarda registradores e/ou label
         // usar o switch para definir exatamente o que terá em cada caso
+        // ex: numa inst do tipo ADD, terá RD, RS e RT
+        // numa inst do tipo BNEZ, terá RS e LABEL
         switch(this.opcode) {
             case "SD":
                 this.type = "S";
@@ -39,10 +40,19 @@ class Instruction {
             case "ADD":
             case "DADDUI":
                 this.type = "R";
+                this.rs = null;
+                this.rt = null;
+                this.rd = null;
                 break;
             case "BEQ":
+                this.rt = null;
+                // caso seja do tipo B, verificar se é BEQ ou BNEZ
+                // antes de presumir que tem RT, ou então poderá dar problema
+                // pois em BNEZ, rt = undefined
             case "BNEZ":
                 this.type = "B";
+                this.rs = null;
+                this.label = null;
                 break;
         }
         this.cycles = config[this.opcode + "cycles"];
@@ -97,6 +107,8 @@ class ReservationStation {
 }
 
 // Unidade de carregamento 
+// talvez não seja necessária, podendo usar
+// uma reservation station "modificada"
 class LoadStoreUnit {
     constructor() {
         this.busy = false;
@@ -143,17 +155,15 @@ class ExeUnit {
         this.store_unit = []; // 3
         this.load_unit = []; // 6
 
-        this.initialize();
-    }
-    initialize() {
+        // inicializar unidades
         for (let i = 0; i < 3; i++) {
             // inicializando unidade de inteiro
             this.integer_unit.push(new ReservationStation()); 
             // inicializando unidade de ponto flutuante - addsub
             this.float_add_sub_unit.push(new ReservationStation());
-            // inicializando unidade de store
+            // inicializando unidade de store - talvez mudar
             this.store_unit.push(new ReservationStation());
-            // inicializando unidade de load
+            // inicializando unidade de load - talvez mudar
             this.load_unit.push(new ReservationStation());
             this.load_unit.push(new ReservationStation());
         }
@@ -162,6 +172,50 @@ class ExeUnit {
             this.float_mult_div_unit.push(new ReservationStation()); 
         }
     }
+
+    add_to_station(instruction) {
+        // auxiliar de issue()
+        if(instruction.type === "R") {
+            for (let i = 0; i < this.integer_unit.length; i++) {
+                if (!this.integer_unit[i].busy()) {
+                    // tem station disponivel
+                    // verificar se os registradores estão sendo utlizados por alguma station
+                    // verificacao feita na variavel registers
+                    return true; // true adicionou
+                }
+                return false; // false nao adicionou
+            }
+        }
+        else if (instruction.type === "L") {}
+        else if (instruction.type === "B") {}
+        else if (instruction.type === "F") {
+            if (instruction.opcode === "MULTD" || instruction.opcode === "DIVD") {
+                // float_mult_div_unit
+            }
+            else {
+                // float_add_sub_unit
+            }
+        }
+        else {
+            // S type
+        }
+    }
+    // nome temporário, funcao que decrementa o tempo das instrucoes que estiverem numa reservation station
+    // de forma a simular a execucao
+    run_stations() {
+        // auxiliar de execute()
+        // percorre todas stations e verifica se pode decrementar
+        // decrementar no caso: simular execucão, passagem de um ciclo
+        // quando chegar a 0 na station, enviar a/as instrucao/os p/ que execute
+        // escreva o ciclo que terminou 
+        // quando chegar em 0, retornar quais instrucoes voltaram para que
+        // a tabela seja atualizada
+    }
+
+    remove_from_station() {
+        // aiuxiliar de write()
+        // 
+    }
 }
 
 // a classe que junta tudo
@@ -169,11 +223,85 @@ class Tomasulo {
     constructor() {
         this.execution_unit = new ExeUnit(); 
         this.instructions = new Queue();
-        read_file(instructions);
+        read_file(this.instructions);
+        this.instructions_w = this.instructions.length(); // guarda numero de isntrucoes que nao foram esctiras
+        this.table = new InstructionTable(); // guarda todas as instrucoes, tabela que exibe onde cada 
+        // table = 2 arrays, um para inst e outro para status: [inst, (...)] e [["ciclo issue", "ciclo exe", "ciclo write"], (...)] 
+        this.cycle = 1;
+        this.finished = false;
     }
-    issue() {};
-    execute() {};
-    write() {};
+    issue(cycle) {
+        var next_inst = this.instructions.peek();
+        if (execution_unit.add_to_station(next_inst)) {
+            this.table.add(next_inst, cycle);
+            this.instructions.pop_front();
+        }
+        /*
+        Retrieve the next instruction from the head of the instruction queue. If the instruction operands are currently in the registers, then
+            If a matching functional unit is available, issue the instruction.
+            Else, as there is no available functional unit, stall the instruction until a station or buffer is free.
+        Otherwise, we can assume the operands are not in the registers, and so use virtual values. The functional unit must calculate the real value to keep track of the functional units that produce the operand.
+        */
+
+    };
+    execute(cycle) {
+        // verifica se eh possivel terminar de executar a instrucao
+        // quando time = 0 na reserv station, significa que terminou de executar
+        // dai limpar a station e adicionar o ciclo na tabela na instrucao
+
+        /*
+        If one or more of the operands is not yet available then: wait for operand to become available on the CDB.
+        When all operands are available, then: if the instruction is a load or store
+            Compute the effective address when the base register is available, and place it in the load/store buffer
+                If the instruction is a load then: execute as soon as the memory unit is available
+                Else, if the instruction is a store then: wait for the value to be stored before sending it to the memory unit
+        Else, the instruction is an arithmetic logic unit (ALU) operation then: execute the instruction at the corresponding functional unit
+        */
+    };
+    write(cycle) {
+        // se conseguir escrever, decrementar 1 de instructions_w
+        for (let i = 0; i < this.table.values; i++) {
+            if (this.tables.status[i].length !== 2) {
+                continue;
+            }
+            // se a execucao terminou no ciclo anterior, significa que devo escrever neste atual
+            if (this.table.status[i][1] === cycle - 1 && this.table.status[i][2] === undefined) {
+                this.table.status[i].push(cycle);
+                /*
+                If the instruction was an ALU operation
+                    If the result is available, then: write it on the CDB and from there into the registers and any reservation stations waiting for this result
+                Else, if the instruction was a store then: write the data to memory during this step
+                */
+                // this.table.instructions[i] -> a instrucao que foi escrita
+                // acessa exe_unit e "limpa" tudo que era dependente da instrucao
+                this.instructions_w--;
+            }
+        }
+    };
+    run() {
+        // cada vez que aperta um botao ele chama run
+        // se ainda houver instruções na fila
+        // e ainda houver isntruções que não foram escritas (instructions_w > 0)
+        if (!this.finished) {
+            // verifica tipo da instrucao
+            // 
+            if (!this.instructions.empty()) {
+                // se fila de isntrucoes não estiver vazia, precisa tentar dar issue 
+                this.issue(cycle);
+            }
+            this.execute(cycle);
+            this.write(cycle);
+
+            // antes de incrementar ciclo, atualizar diagramas das reservation stations e register status
+            // TODO: funcao para atualizar diagramas
+            
+            if (!this.instructions.empty() && this.instructions_w > 0) {
+                cycle++;
+            } else {
+                this.finished = true;
+            }
+        }
+    }
 }
 
 class Queue {
@@ -227,5 +355,11 @@ function help() {
     a = "Dicas de uso: \nPara inserir uma intrução na unidade escolha as instruções a serem utilizadas e seus respectivos registradores, então clique em 'CONFIRMAR' para executar os dados ou 'RESET' para limpar os campos\n\nO botão 'Próximo' avançará para o proximo ciclo e o botão 'Resultado' apresenta o resultado final da execução do algoritmo";
     alert(a);
 }
+
+// modo lento
+// on button click: t.run()
+
+// modo rapido
+// while (!t.over): t.run()
 
 // pega instruction da entrada, joga para a reservation station e executa
